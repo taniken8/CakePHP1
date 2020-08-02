8376f63087a22d8b96c44c4758cff6244129b3b6
taniken8 <54733824+taniken8@users.noreply.github.com>
2020-08-02 18:30:55 +0900

    インデントをスペースに変換

--- src/Controller/Component/CommonComponent.php
+++ src/Controller/Component/CommonComponent.php
@@ -17,74 +17,74 @@ class CommonComponent extends Component
      *
      * @var array
      */
-	protected $_defaultConfig = [];
+    protected $_defaultConfig = [];
 
 
 
-	public function initialize(array $config): void
-	{
-		$this->controller = $this->_registry->getController();
-		$this->controller->loadModel('Auction');
-	}
+    public function initialize(array $config): void
+    {
+        $this->controller = $this->_registry->getController();
+        $this->controller->loadModel('Auction');
+    }
 
-	//連絡先を送っているかどうか
-	public function isContact($id)
-	{
-		$bidinfo = $this->controller->Bidinfo->get($id, [
-			'contain' => ['Bidcontacts']]);
+    //連絡先を送っているかどうか
+    public function isContact($id)
+    {
+        $bidinfo = $this->controller->Bidinfo->get($id, [
+            'contain' => ['Bidcontacts']]);
 
-		//連絡先を送っていればfalse
-		if ($bidinfo->bidcontact) {
-			return false;
-		}
-		return true;
-	}
+        //連絡先を送っていればfalse
+        if ($bidinfo->bidcontact) {
+            return false;
+        }
+        return true;
+    }
 
-	//発送ボタンを押したかどうか
-	public function isShipping($id)
-	{
-		$bidinfo = $this->controller->Bidinfo->get($id, [
-			'contain' => ['Bidcontacts']]);
+    //発送ボタンを押したかどうか
+    public function isShipping($id)
+    {
+        $bidinfo = $this->controller->Bidinfo->get($id, [
+            'contain' => ['Bidcontacts']]);
 
-		//連絡先を送っていなければfalse
-		if (!$bidinfo->bidcontact) {
-			return false;
-		}
-		//発送ボタンが押されていればfalse
-		if ($bidinfo->bidcontact->send) {
-			return false;
-		}
-		return true;
-	}
+        //連絡先を送っていなければfalse
+        if (!$bidinfo->bidcontact) {
+            return false;
+        }
+        //発送ボタンが押されていればfalse
+        if ($bidinfo->bidcontact->send) {
+            return false;
+        }
+        return true;
+    }
 
-	//受取ボタンを押したかどうか
-	public function isReceipt($id)
-	{
-		$bidinfo = $this->controller->Bidinfo->get($id, [
-			'contain' => ['Bidcontacts']]);
+    //受取ボタンを押したかどうか
+    public function isReceipt($id)
+    {
+        $bidinfo = $this->controller->Bidinfo->get($id, [
+            'contain' => ['Bidcontacts']]);
 
-		$bidcontact = $bidinfo->bidcontact;
+        $bidcontact = $bidinfo->bidcontact;
 
-		//連絡先を送っていなければfalse
-		if (!$bidcontact) {
-			return false;
-		}
-		//発送ボタンが押されていなければfalse
-		if (!$bidcontact->send) {
-			return false;
-		}
-		//受取ボタンが押されていればfalse
-		if ($bidcontact->receipt) {
-			return false;
-		}
-		return true;
-	}
+        //連絡先を送っていなければfalse
+        if (!$bidcontact) {
+            return false;
+        }
+        //発送ボタンが押されていなければfalse
+        if (!$bidcontact->send) {
+            return false;
+        }
+        //受取ボタンが押されていればfalse
+        if ($bidcontact->receipt) {
+            return false;
+        }
+        return true;
+    }
 
-	//評価をしているかどうか
-	public function isReview($id)
-	{
-		$bidinfo = $this->controller->Bidinfo->get($id, [
-			'contain' => ['Bidcontacts']]);
+    //評価をしているかどうか
+    public function isReview($id)
+    {
+        $bidinfo = $this->controller->Bidinfo->get($id, [
+            'contain' => ['Bidcontacts']]);
         $bidreview = $this->controller->Bidreviews->find()
             ->where([
                 'bidinfo_id' => $id,
@@ -92,32 +92,32 @@ class CommonComponent extends Component
             ])
             ->count();
 
-		$bidcontact = $bidinfo->bidcontact;
+        $bidcontact = $bidinfo->bidcontact;
 
-		//連絡先を送っていなければfalse
-		if (!$bidcontact) {
-			return false;
-		}
-		//発送ボタンが押されていなければfalse
-		if (!$bidcontact->send) {
-			return false;
-		}
-		//受取ボタンを押されていなければfalse
-		if (!$bidcontact->receipt) {
-			return false;
-		}
-		//ログインユーザーが評価していればfalse
-		if ($bidreview === 1) {
-			return false;
-		}
-		return true;
-	}
+        //連絡先を送っていなければfalse
+        if (!$bidcontact) {
+            return false;
+        }
+        //発送ボタンが押されていなければfalse
+        if (!$bidcontact->send) {
+            return false;
+        }
+        //受取ボタンを押されていなければfalse
+        if (!$bidcontact->receipt) {
+            return false;
+        }
+        //ログインユーザーが評価していればfalse
+        if ($bidreview === 1) {
+            return false;
+        }
+        return true;
+    }
 
-	//取引が完了しているかどうか
-	public function isFinish($id)
-	{
-		$bidinfo = $this->controller->Bidinfo->get($id, [
-			'contain' => ['Bidcontacts']]);
+    //取引が完了しているかどうか
+    public function isFinish($id)
+    {
+        $bidinfo = $this->controller->Bidinfo->get($id, [
+            'contain' => ['Bidcontacts']]);
         $bidreview = $this->controller->Bidreviews->find()
             ->where([
                 'bidinfo_id' => $id,
@@ -125,24 +125,24 @@ class CommonComponent extends Component
             ])
             ->count();
 
-		$bidcontact = $bidinfo->bidcontact;
+        $bidcontact = $bidinfo->bidcontact;
 
-		//連絡先を送っていなければfalse
-		if (!$bidcontact) {
-			return false;
-		}
-		//発送ボタンが押されていなければfalse
-		if (!$bidcontact->send) {
-			return false;
-		}
-		//受取ボタンを押されていなければfalse
-		if (!$bidcontact->receipt) {
-			return false;
-		}
-		//ログインユーザーが評価されていなければfalse
-		if (!$bidreview === 1) {
-			return false;
-		}
-		return true;
-	}
+        //連絡先を送っていなければfalse
+        if (!$bidcontact) {
+            return false;
+        }
+        //発送ボタンが押されていなければfalse
+        if (!$bidcontact->send) {
+            return false;
+        }
+        //受取ボタンを押されていなければfalse
+        if (!$bidcontact->receipt) {
+            return false;
+        }
+        //ログインユーザーが評価されていなければfalse
+        if (!$bidreview === 1) {
+            return false;
+        }
+        return true;
+    }
 }
